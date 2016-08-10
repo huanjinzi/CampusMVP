8fa0a41bde34c76b2b8cac201bc66959fa686344
huanjinzi <huanjinzi@live.com>
2016-08-10 14:24:27 +0800

    Inputstream的读取，返回字符串

---
+++ app/src/main/java/com/campus/huanjinzi/campusmvp/http/HjzStreamReader.java
@@ -0,0 +1,32 @@
+package com.campus.huanjinzi.campusmvp.http;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by huanjinzi on 2016/8/10.
+ */
+public class HjzStreamReader {
+
+    public static StringBuilder getString(InputStream in) throws Exception {
+        return getString(in, "utf-8", -1);
+    }
+
+    public static StringBuilder getString(InputStream in, String charset) throws Exception {
+        return getString(in, charset, -1);
+    }
+
+    public static StringBuilder getString(InputStream in, String charset, int StreamLenth) throws Exception {
+
+        int len = 0;
+        char[] buf = new char[1024];
+        StringBuilder sb = new StringBuilder();
+        InputStreamReader reader = new InputStreamReader(in, charset);
+
+        while ((len = reader.read(buf)) != -1) {
+            sb.append(buf, 0, len);
+            // TODO: 2016/8/10 进度读取，不需要知道返回值的长度 
+        }
+        return sb;
+    }
+}
