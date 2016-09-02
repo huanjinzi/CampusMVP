cb0abe59f7c7860bf3ac30cfef7438a3028cc0f9
huanjinzi <huanjinzi@live.com>
2016-09-02 20:38:57 +0800

    两个圆圈，缩放效果，实现单击和长按ripple效果及相应的事件

--- app/src/main/java/com/campus/huanjinzi/campusmvp/view/customview/ProgressView.java
+++ app/src/main/java/com/campus/huanjinzi/campusmvp/view/customview/ProgressView.java
@@ -2,40 +2,45 @@ package com.campus.huanjinzi.campusmvp.view.customview;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by huanjinzi on 2016/9/1.
  */
-public class ProgressView extends View{
+public class ProgressView extends View {
 
     private Paint mpaint;
-    private int i = 0;
+    private boolean show = true;
     private boolean flag = false;
+    private boolean drawcircle = false;
+    private boolean drawrect = false;
+    private OnClickListener mOnClickListener;
+    private OnLongClickListener mOnLongClickListener;
 
     private Handler mHandler;
-    private void init(){
+
+    private void init() {
+
         mpaint = new Paint();
-        mpaint.setStyle(Paint.Style.STROKE);
-        mpaint.setColor(Color.WHITE);
-        mpaint.setStrokeWidth(4);
         mpaint.setAntiAlias(true);
-        mHandler = new Handler(){
+        mHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
                 invalidate();
-                mHandler.sendEmptyMessageDelayed(0,25);
+                mHandler.sendEmptyMessageDelayed(0, 25);
             }
         };
-        mHandler.sendEmptyMessageDelayed(0,500);
+        mHandler.sendEmptyMessageDelayed(0, 400);
 
     }
+
     public ProgressView(Context context) {
         super(context);
         init();
@@ -46,33 +51,125 @@ public class ProgressView extends View{
         init();
     }
 
+
+    private int i = 0;
+    private int j = 10;//点击控件初始圆的大小，默认j*j=100
+    private float x = 0;
+    private float y = 0;
+
     @Override
     protected void onDraw(Canvas canvas) {
-
-        if(i >50)
-        {
+        /**控制是放大还是缩小，i = 40，说明已到最大，i需要减小；i = 0则相反*/
+        if (i > 40) {
             flag = true;
         }
-        if(i == 0)
-        {
+        if (i == 0) {
             flag = false;
         }
 
-        if(flag)
-        {
-            mpaint.setStrokeWidth(2);
-            canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 90 + i/3, mpaint);
-            mpaint.setStrokeWidth(8);
-            canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 40 + i, mpaint);
-            i-=1;
+        /**缩放*/
+        if (show) {
+
+            mpaint.setStyle(Paint.Style.STROKE);
+            mpaint.setColor(0xddffffff);
+            if (flag) {
+                mpaint.setStrokeWidth(2);
+                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 90 + i / 3, mpaint);//大圆圈缩小
+                mpaint.setStrokeWidth(8);
+                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 40 + i, mpaint);//小圆圈缩小
+                i -= 1;
+            } else {
+                mpaint.setStrokeWidth(2);
+                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 90 + i / 3, mpaint);//大圆圈放大
+                mpaint.setStrokeWidth(8);
+                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 40 + i, mpaint);//小圆圈放大
+                i += 1;
+            }
         }
+        /**椭圆缩放，上下缩放*/
         else
         {
-            mpaint.setStrokeWidth(2);
-            canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 90 + i/3, mpaint);
-            mpaint.setStrokeWidth(8);
-            canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 40 + i, mpaint);
-            i+=1;
+            mpaint.setStyle(Paint.Style.STROKE);
+            mpaint.setColor(0xddffffff);
+            if (flag) {
+                mpaint.setStrokeWidth(2);
+                canvas.drawOval(canvas.getWidth()/2-90-i, canvas.getHeight()/2-90-i/5, canvas.getWidth()/2+90+i,canvas.getHeight()/2+90+i/5, mpaint);//大圆圈缩小
+                mpaint.setStrokeWidth(8);
+                canvas.drawOval(canvas.getWidth()/2-40-i, canvas.getHeight()/2-40-i*2, canvas.getWidth()/2+40+i,canvas.getHeight()/2+40+i*2, mpaint);////小圆圈缩小
+                i -= 1;
+            } else {
+                mpaint.setStrokeWidth(2);
+                canvas.drawOval(canvas.getWidth()/2-90-i, canvas.getHeight()/2-90-i/5, canvas.getWidth()/2+90+i,canvas.getHeight()/2+90+i/5, mpaint);//大圆圈放大
+                mpaint.setStrokeWidth(8);
+                canvas.drawOval(canvas.getWidth()/2-40-i, canvas.getHeight()/2-40-i*2, canvas.getWidth()/2+40+i,canvas.getHeight()/2+40+i*2, mpaint);//小圆圈放大
+                i += 1;
+            }
         }
+
+
+
+        mpaint.setStyle(Paint.Style.FILL);
+        if (drawcircle) {
+            mpaint.setColor(0x22ffffff);
+            canvas.drawCircle(x, y, j * j, mpaint);
+
+        }
+        if (j < 35) {
+            j++;
+        }
+
+        if (drawrect) {
+            mpaint.setColor(0x11ffffff);
+            canvas.drawRect(0, 0, canvas.getWidth(), getHeight(), mpaint);
+
+        }
+
+    }
+
+
+    private long time = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (clickable) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    time = System.currentTimeMillis();
+                    j = 10;
+                    x = event.getX();
+                    y = event.getY();
+                    drawcircle = true;
+                    drawrect = true;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    time = System.currentTimeMillis() - time;
+                    if (time < 300) { mOnClickListener.onClick(this); }
+                    if(time > 1000){mOnLongClickListener.onLongClick(this);}
+                    drawcircle = false;
+                    drawrect = false;
+                    break;
+            }
+            return true;
+        } else {
+            return super.onTouchEvent(event);
+        }
+    }
+
+    @Override
+    public void setOnClickListener(OnClickListener l) {
+        mOnClickListener = l;
+    }
+
+    @Override
+    public void setOnLongClickListener(OnLongClickListener l) {mOnLongClickListener = l;}
+
+    private boolean clickable = true;
+    public void Clickable(boolean clickable) {
+        this.clickable = clickable;
+    }
+
+    public void setShow(boolean show) {
+        this.show = show;
     }
 }
