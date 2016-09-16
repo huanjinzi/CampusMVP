27c4375e65c255baeea54bdab4b1fb8e0413f31c
huanjinzi <huanjinzi@live.com>
2016-09-16 19:21:43 +0800

    水波纹效果

--- app/src/main/java/com/campus/huanjinzi/campusmvp/view/customview/ProgressView.java
+++ app/src/main/java/com/campus/huanjinzi/campusmvp/view/customview/ProgressView.java
@@ -2,7 +2,9 @@ package com.campus.huanjinzi.campusmvp.view.customview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
@@ -15,7 +17,6 @@ import android.view.View;
 public class ProgressView extends View {
 
     private Paint mpaint;
-    private boolean show = true;
     private boolean flag = false;
     private boolean drawcircle = false;
     private boolean drawrect = false;
@@ -50,13 +51,19 @@ public class ProgressView extends View {
     }
 
 
-    private int i = 0;
+    private float i = 0;
     private int j = 20;//点击控件初始圆的大小，默认j*j=100
+    private int k = 0;
+
+    private int sin_a = 50;
+    private int base_line = 150;
     private float x = 0;
     private float y = 0;
 
     @Override
     protected void onDraw(Canvas canvas) {
+        float Height = canvas.getHeight();
+        float Width = canvas.getWidth();
         /**控制是放大还是缩小，i = 40，说明已到最大，i需要减小；i = 0则相反*/
         if (i > 40) {
             flag = true;
@@ -64,67 +71,66 @@ public class ProgressView extends View {
         if (i == 0) {
             flag = false;
         }
-
         /**缩放*/
-        if (show) {
-
-            mpaint.setStyle(Paint.Style.STROKE);
-            mpaint.setColor(0xddffffff);
-            if (flag) {
-                mpaint.setStrokeWidth(2);
-                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 90 + i / 3, mpaint);//大圆圈缩小
-                mpaint.setStrokeWidth(8);
-                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 40 + i, mpaint);//小圆圈缩小
-                i -= 1;
-            } else {
-                mpaint.setStrokeWidth(2);
-                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 90 + i / 3, mpaint);//大圆圈放大
-                mpaint.setStrokeWidth(8);
-                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 40 + i, mpaint);//小圆圈放大
-                i += 1;
-            }
+        mpaint.setStyle(Paint.Style.STROKE);
+        mpaint.setColor(0xddffffff);
+        if (flag) {
+            mpaint.setStrokeWidth(2);
+            canvas.drawCircle(Width / 2, Height / 2, 90 + i / 6, mpaint);//大圆圈缩小
+            mpaint.setStrokeWidth(8);
+            canvas.drawCircle(Width / 2, Height / 2, 40 + i / 2, mpaint);//小圆圈缩小
+            i -= 0.5;
+        } else {
+            mpaint.setStrokeWidth(2);
+            canvas.drawCircle(Width / 2, Height / 2, 90 + i / 6, mpaint);//大圆圈放大
+            mpaint.setStrokeWidth(8);
+            canvas.drawCircle(Width / 2, Height / 2, 40 + i / 2, mpaint);//小圆圈放大
+            i += 0.5;
         }
-        /**椭圆缩放，上下缩放*/
-        else
-        {
-            mpaint.setStyle(Paint.Style.STROKE);
-            mpaint.setColor(0xddffffff);
-            if (flag) {
-                mpaint.setStrokeWidth(2);
-                canvas.drawOval(canvas.getWidth()/2-90-i, canvas.getHeight()/2-90-i/5, canvas.getWidth()/2+90+i,canvas.getHeight()/2+90+i/5, mpaint);//大圆圈缩小
-                mpaint.setStrokeWidth(8);
-                canvas.drawOval(canvas.getWidth()/2-40-i, canvas.getHeight()/2-40-i*2, canvas.getWidth()/2+40+i,canvas.getHeight()/2+40+i*2, mpaint);////小圆圈缩小
-                i -= 1;
-            } else {
-                mpaint.setStrokeWidth(2);
-                canvas.drawOval(canvas.getWidth()/2-90-i, canvas.getHeight()/2-90-i/5, canvas.getWidth()/2+90+i,canvas.getHeight()/2+90+i/5, mpaint);//大圆圈放大
-                mpaint.setStrokeWidth(8);
-                canvas.drawOval(canvas.getWidth()/2-40-i, canvas.getHeight()/2-40-i*2, canvas.getWidth()/2+40+i,canvas.getHeight()/2+40+i*2, mpaint);//小圆圈放大
-                i += 1;
-            }
-        }
-
-
 
         mpaint.setStyle(Paint.Style.FILL);
+        /**单击时的效果*/
         if (drawcircle) {
             mpaint.setColor(0x22ffffff);
             canvas.drawCircle(x, y, j * 5, mpaint);
-
         }
         if (j < 250) {
-            j+=2;
+            j += 2;
         }
 
         if (drawrect) {
             mpaint.setColor(0x11ffffff);
-            canvas.drawRect(0, 0, canvas.getWidth(), getHeight(), mpaint);
-
+            canvas.drawRect(0, 0, Width, getHeight(), mpaint);
         }
-
+        /**正在登陆的波浪效果*/
+        mpaint.setColor(0x33ffffff);
+        mpaint.setStyle(Paint.Style.FILL);
+        mpaint.setStrokeWidth(2);
+        drawSin(canvas, Width, Height);
     }
 
+    private void drawSin(Canvas canvas, float Width, float Height) {
+        if (true) {
 
+            if (k <= -Width) {
+                k = 0;
+            }
+            Path path = new Path();
+            path.moveTo(k, Height - base_line);
+
+            /**整体偏移j个位移*/
+            path.quadTo(Width / 4 + k, Height - base_line - sin_a, Width / 2 + k, Height - base_line);
+            path.quadTo(Width / 4 * 3 + k, Height - base_line + sin_a, Width + k, Height - base_line);
+
+            path.quadTo(Width / 4 * 5 + k, Height - base_line - sin_a, Width / 2 * 3 + k, Height - base_line);
+            path.quadTo(Width / 4 * 7 + k, Height - base_line + sin_a, Width * 2 + k, Height - base_line);
+            path.lineTo(Width*2,Height);
+            path.lineTo(0,Height);
+            path.close();
+            canvas.drawPath(path, mpaint);
+            k -= 6;
+        }
+    }
     private long time = 0;
 
     @Override
@@ -142,9 +148,11 @@ public class ProgressView extends View {
                     break;
                 case MotionEvent.ACTION_UP:
                     time = System.currentTimeMillis() - time;
-                    if (time < 300) { mOnClickListener.onClick(this); }
-                    if(time > 1000){
-                        if(null != mOnLongClickListener){
+                    if (time < 300) {
+                        mOnClickListener.onClick(this);
+                    }
+                    if (time > 1000) {
+                        if (null != mOnLongClickListener) {
                             mOnLongClickListener.onLongClick(this);
                         }
                     }
@@ -157,23 +165,21 @@ public class ProgressView extends View {
             return super.onTouchEvent(event);
         }
     }
-
     @Override
     public void setOnClickListener(OnClickListener l) {
         mOnClickListener = l;
     }
 
     @Override
-    public void setOnLongClickListener(OnLongClickListener l) {mOnLongClickListener = l;}
-
+    public void setOnLongClickListener(OnLongClickListener l) {
+        mOnLongClickListener = l;
+    }
 
 
     private boolean clickable = true;
+
     public void Clickable(boolean clickable) {
         this.clickable = clickable;
     }
 
-    public void setShow(boolean show) {
-        this.show = show;
-    }
 }
